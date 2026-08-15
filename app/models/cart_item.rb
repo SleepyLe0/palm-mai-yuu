34d65f12b8f664ad33047fa784d231f2ed18ac64
Jinnawat Vilairat <github@lynchz.dev>
2026-08-15 14:15:44 +0700

    can't by more than stock quantitiy

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -4,4 +4,17 @@ class CartItem < ApplicationRecord
 
   validates :quantity, numericality: { only_integer: true, greater_than: 0 }
   validates :product_id, uniqueness: { scope: :cart_id }
+  validate :quantity_within_stock
+
+  private
+
+  def quantity_within_stock
+    return if product.nil? || quantity.nil?
+
+    if product.stock_count.to_i.zero?
+      errors.add(:base, "#{product.name} is out of stock")
+    elsif quantity > product.stock_count
+      errors.add(:quantity, "only #{product.stock_count} of #{product.name} left in stock")
+    end
+  end
 end
