b41ea1c3e0acd097d9d602e92c8d99ad337e6abe
Jinnawat Vilairat <github@lynchz.dev>
2026-08-15 14:44:54 +0700

    แก้ของเก่าจ้า เอาออก

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -4,17 +4,8 @@ class CartItem < ApplicationRecord
 
   validates :quantity, numericality: { only_integer: true, greater_than: 0 }
   validates :product_id, uniqueness: { scope: :cart_id }
-  validate :quantity_within_stock
 
-  private
-
-  def quantity_within_stock
-    return if product.nil? || quantity.nil?
-
-    if product.stock_count.to_i.zero?
-      errors.add(:base, "#{product.name} is out of stock")
-    elsif quantity > product.stock_count
-      errors.add(:quantity, "only #{product.stock_count} of #{product.name} left in stock")
-    end
+  def exceeds_stock?
+    quantity > product.stock_count.to_i
   end
 end
